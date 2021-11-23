d2008b634614c7516c60e06954c9ceb53e339a12
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2021-11-23 15:43:08 -0800

    Atou works
    drive linar works
    it is reversed but it works

--- CODE/src/main/java/frc/robot/commands/DrivePID.java
+++ CODE/src/main/java/frc/robot/commands/DrivePID.java
@@ -7,8 +7,11 @@ import frc.robot.Constants.DriveConstants;
 
 public class DrivePID extends CommandBase {
     private final DriveSubsystem m_drive;
-    int current = 0;
-    double goal; 
+    double error = 0;
+    double goal;
+    double startingLeft;
+    double startingRight;
+    boolean finsihed;
 
     public DrivePID(DriveSubsystem subsystem, double goal_) {
         m_drive = subsystem;
@@ -26,36 +29,39 @@ public class DrivePID extends CommandBase {
   
     // Called when the command is initially scheduled.
     public void initialize() {
-      current = 0;
-      // 0 encoders 
+      finsihed = false;
+      // 0 encoders or replacement get starting poitions
+      startingLeft = m_drive.getPositionLeft();
+      startingRight = m_drive.getPositionRight();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     public void execute() {
       double maxOutput = 0.2;
-      double maxError = 2;
-      double error = goal - (m_drive.getPositionLeft()+m_drive.getPositionRight());
-      System.out.println(m_drive.getPositionLeft()+m_drive.getPositionRight());
+      double maxError = 25;
+      
+      error = goal - ((m_drive.getPositionLeft()- startingLeft) + (m_drive.getPositionRight()-startingRight))/2;
+      
       double p_gain = 0.1;
       double finalVal = error * p_gain;
       if (finalVal > maxOutput){
         finalVal = maxOutput;
       }if (finalVal < -maxOutput) {
         finalVal = -maxOutput;
-      }if (maxError > error && error > maxError) {
+      }if (maxError > error && error > -maxError) {
         finalVal = 0;
+        finsihed = true;
       }
       m_drive.tankDrive(finalVal,finalVal);
     }
     
     // Called once the command ends or is interrupted.
     public void end(boolean interrupted) {
-        current = 0;
     }
 
     // Returns true when the command should end.
     public boolean isFinished() {
         // return false;
-        return (current > goal);
+        return (finsihed);
     }
   }
